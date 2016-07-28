7ca46fbe4c6dfd85cd9e58a6e29b129273d6e354
Maksim <m.ganakov@belitsoft.by>
2016-07-28 17:18:41 +0300

    update2

--- com_joomlaquiz/admin/sql/updates/mysql/3.5.0.005.sql
+++ com_joomlaquiz/admin/sql/updates/mysql/3.5.0.005.sql
@@ -1,5 +1,1 @@
 ALTER TABLE `#__quiz_certificates` ADD COLUMN `cert_offset` INT(4) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `text_font`;
-
-UPDATE `#__quiz_certificates` SET `cert_offset` = 100 WHERE `#__quiz_certificates`.`id` = 1;
-UPDATE `#__quiz_certificates` SET `cert_offset` = 150 WHERE `#__quiz_certificates`.`id` = 2;
-UPDATE `#__quiz_certificates` SET `cert_offset` = 200 WHERE `#__quiz_certificates`.`id` = 3;

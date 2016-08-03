563a841bf52ab46684acac7e20cef2f7c524e9f8
Maksim <m.ganakov@belitsoft.by>
2016-08-03 12:14:22 +0300

    cert_update

--- com_joomlaquiz/admin/sql/updates/mysql/3.5.0.005.sql
+++ com_joomlaquiz/admin/sql/updates/mysql/3.5.0.005.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `#__quiz_certificates` ADD COLUMN `cert_offset` INT(4) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `text_font`;
+ALTER TABLE `#__quiz_certificates` ADD COLUMN `cert_offset` INT(4) NOT NULL AFTER `text_font`;

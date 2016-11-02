1fe8af67d26b49cbf46f2283a9a05a248b963a60
Alexandr <a.kosarev@belitsoft.com>
2016-11-02 18:43:43 +0300

    sql and ajax fixes

--- com_joomlaquiz/admin/sql/updates/mysql/3.6.1.000.sql
+++ com_joomlaquiz/admin/sql/updates/mysql/3.6.1.000.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `#__quiz_r_student_quiz` ADD COLUMN `time_left` INT(11) NOT NULL DEFAULT '0' AFTER `params`;
+ALTER TABLE `#__quiz_r_student_quiz` ADD COLUMN `time_past` INT(11) NOT NULL DEFAULT '0' AFTER `params`;
+ALTER TABLE `#__quiz_r_student_quiz` ADD COLUMN `respond_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP AFTER `params`;

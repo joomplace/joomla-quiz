166d6c3ba88077646d446d46f02bdbf049fd5bba
Alexandr Kosarev <a.kosarev@belitsoft.com>
2016-11-15 13:55:09 +0300

    catch and screenshots

--- tests/acceptance/InstallComponentCest.php
+++ tests/acceptance/InstallComponentCest.php
@@ -38,8 +38,42 @@ class InstallComponentCest
 		$I->attachFile('input#install_package','pkg.zip');
 		$I->click('#installbutton_package');
 		$I->comment('I will wait while installing, but 60 seconds max');
-		$I->waitForElement('.alert.alert-success', 120);
-		$I->dontSee('.alert.alert-error');
-		$I->dontSee('.alert.alert-warning');
+		try {
+			$I->waitForElement('.alert.alert-success', 120);
+			$I->dontSee('.alert.alert-error');
+			$I->dontSee('.alert.alert-warning');
+			$I->makeScreenshot('install_successMobile');
+			$this->goLaptop($I);
+			$I->makeScreenshot('install_successLaptop');
+			$this->goiPad($I);
+			$I->makeScreenshot('install_successiPad');
+		}catch (\Exception $e) {
+			$I->makeScreenshot('install_failed');
+			$I->comment('Extention was not installed');
+			throw $e;
+		}
+	}
+
+	protected function goLaptop(AcceptanceTester $I){
+		$size = array('1258', '605');
+		call_user_func_array(array($I,'resizeWindow'),$size);
+	}
+
+	protected function goMobile(AcceptanceTester $I, $mode = 'portrait'){
+		// NEXUS 5
+		$size = array('412', '732');
+		if($mode != 'portrait'){
+			array_reverse($size);
+		}
+		call_user_func_array(array($I,'resizeWindow'),$size);
+	}
+
+	protected function goiPad(AcceptanceTester $I, $mode = 'portrait'){
+		// iPad
+		$size = array('768','1024');
+		if($mode != 'portrait'){
+			array_reverse($size);
+		}
+		call_user_func_array(array($I,'resizeWindow'),$size);
 	}
 }

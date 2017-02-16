61b5c2fe27a0ff03c12c371885264571d0f1c83a
TsaiEvgenii <tsai.evgenii@gmail.com>
2017-02-16 12:00:29 +0300

    Hide current and latest version

--- com_joomlaquiz/admin/views/dashboard/tmpl/default.php
+++ com_joomlaquiz/admin/views/dashboard/tmpl/default.php
@@ -57,21 +57,22 @@ function subfolding($r_url){
                             <a href="http://www.JoomPlace.com">JoomPlace</a>.
                         </th>
                     </tr>
-                    <tr>
-                        <td width="13%"  align="left">Installed version:</td>
-                        <td align="left">&nbsp;<b><?php echo JoomlaquizHelper::getVersion();?></b>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="left">Latest version:</td>
-                        <td>
-                            <div id="jq_LatestVersion">
-                                <script type="text/javascript">
-                                    jq_CheckVersion();
-                                </script>
-                            </div>
-                        </td>
-                    </tr>
+                    <!-- Hide current and latest version since integrate Joomla update system -->
+<!--                    <tr>-->
+<!--                        <td width="13%"  align="left">Installed version:</td>-->
+<!--                        <td align="left">&nbsp;<b>--><?php //echo JoomlaquizHelper::getVersion();?><!--</b>-->
+<!--                        </td>-->
+<!--                    </tr>-->
+<!--                    <tr>-->
+<!--                        <td align="left">Latest version:</td>-->
+<!--                        <td>-->
+<!--                            <div id="jq_LatestVersion">-->
+<!--                                <script type="text/javascript">-->
+<!--                                    jq_CheckVersion();-->
+<!--                                </script>-->
+<!--                            </div>-->
+<!--                        </td>-->
+<!--                    </tr>-->
                     <tr>
                         <td valign="top" align="left">About:</td>
                         <td align="left"><?php echo JText::_('COM_JOOMLAQUIZ_ABOUT_TEXT'); ?></td>

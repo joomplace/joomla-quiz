9c0034d8813d41538ce87e278c32a3fe442c3961
imnakez <imnakez@yandex.ru>
2017-06-19 14:36:54 +0300

    Merge branch '3.7' of http://git.joomplace.com/joomplace/quiz into custom-541
    # Conflicts:
    #	com_joomlaquiz/joomlaquiz.xml
    #	com_joomlaquiz_expert/joomlaquiz.xml
    #	content_quizcustomfields/quizcustomfields.xml
    #	easysocial_myquizzez_app/myquizzez.xml
    #	jq_alphauserpoints/jq_alphauserpoints.xml
    #	jq_altauserpoints/jq_altauserpoints.xml
    #	jq_cb_myquizzes/cb_myquizzes.xml
    #	mod_quiz_lastten/mod_quiz_lastten.xml
    #	mod_quiz_myresult/mod_quiz_myresult.xml
    #	mod_quiz_topxquiz/mod_quiz_topxquiz.xml
    #	mod_quiz_topxusers/mod_quiz_topxusers.xml
    #	plg_blank/blank.xml
    #	plg_boilerplate/boilerplate.xml
    #	plg_choice/choice.xml
    #	plg_dalliclick/dalliclick.xml
    #	plg_dragdrop/dragdrop.xml
    #	plg_dropdown/dropdown.xml
    #	plg_hotspot/hotspot.xml
    #	plg_imgmatch/imgmatch.xml
    #	plg_memory/memory.xml
    #	plg_mquestion/mquestion.xml
    #	plg_mresponse/mresponse.xml
    #	plg_puzzle/puzzle.xml
    #	plg_smart_search/joomlaquiz.xml
    #	plg_surveys/surveys.xml
    #	plg_truefalse/truefalse.xml
    #	plgup_joomlaquizpoints/plgup_joomlaquizpoints.xml
    #	quiz-xtd-button/quiz.xml
    #	quiz_content_plugin/quizcont.xml

--- plg_choice/choice.php
+++ plg_choice/choice.php
@@ -373,7 +373,7 @@ class plgJoomlaquizChoice extends plgJoomlaquizQuestion
 					
 			$data['pdf']->Ln();
 
-			$data['pdf']->setFont($fontFamily);
+			$data['pdf']->setFont($fontFamily, 'B');
 			//$data['pdf']->setStyle('b', true);
 			$str = "  $k.";
 			$data['pdf']->Write(5, $data['pdf_doc']->cleanText($str), '', 0);
@@ -386,7 +386,7 @@ class plgJoomlaquizChoice extends plgJoomlaquizQuestion
 		}
 
 		$data['pdf']->Ln();
-		$data['pdf']->setFont($fontFamily);
+		$data['pdf']->setFont($fontFamily, 'B');
 		//$data['pdf']->setStyle('b', true);
 		$str = '  '.JText::_('COM_QUIZ_PDF_ANSWER');
 		$data['pdf']->Write(5, $data['pdf_doc']->cleanText($str), '', 0);
@@ -394,7 +394,13 @@ class plgJoomlaquizChoice extends plgJoomlaquizQuestion
 		//$data['pdf']->setStyle('b', false);
 		$str = $data['answer'];
 		$data['pdf']->Write(5, $data['pdf_doc']->cleanText($str), '', 0);
-		
+
+		if ($data['data']["is_correct"]) {
+			$str = JText::_('COM_QUIZ_PDF_ANSWER_CORRECT');
+		} else {
+			$str = JText::_('COM_QUIZ_PDF_ANSWER_INCORRECT');
+		}
+		$data['pdf']->Write(5, " ".$data['pdf_doc']->cleanText($str), '', 0);
 		return $data['pdf'];		
 	}
 	

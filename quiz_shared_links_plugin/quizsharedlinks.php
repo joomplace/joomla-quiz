ab3c93bc4b9df59760efb12655d3c717f81e0d58
Alexey Palshin <a.palshin@belitsoft.com>
2017-07-20 11:46:00 +0300

    added plugin which replace links on social icons

---
+++ quiz_shared_links_plugin/quizsharedlinks.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * JoomlaQuiz plugin for Joomla
+ * @version $Id: quizsharedlinks.php 2011-03-03 17:30:15
+ * @package JoomlaQuiz
+ * @subpackage quizsharedlinks.php
+ * @author JoomPlace Team
+ * @copyright Copyright (C) JoomPlace, www.joomplace.com
+ * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+class plgContentQuizSharedLinks extends JPlugin {
+
+	function onContentPrepare($context, &$article, &$params, $limitstart)
+	{
+        // simple performance check to determine whether bot should process further
+        if (strpos($article->text, 'quiz_results') === false) {
+            return true;
+        }
+
+        $regex = '/task%3Dresults.sturesult%26id%3D(\d+)%26share_id%3D(\d+)/';
+
+        /**
+         * $matches[1] contains quiz id
+         * $matches[2] contains share_id
+         */
+        preg_match_all($regex, $article->text, $matches);
+
+        $share_link = 'view%3Dquiz%26';
+
+        $article->text = str_replace($matches[0], $share_link, $article->text);
+
+        return true;
+    }
+}
+
+?>

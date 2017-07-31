c5527278a09012087fddbda2c01fd823e93ed094
TsaiEvgenii <tsai.evgenii@gmail.com>
2017-07-31 10:00:51 +0300

    Changed the rights check

--- com_joomlaquiz/site/models/results.php
+++ com_joomlaquiz/site/models/results.php
@@ -142,7 +142,7 @@ class JoomlaquizModelResults extends JModelList
 				->join('left', $db->qn('#__quiz_t_quiz','q').' ON '.$db->qn('sq.c_quiz_id').' = '.$db->qn('q.c_id'))
 				->where($db->qn('sq.c_quiz_id').' = '.$db->q($row->c_quiz_id));
 			
-			if(!$user->authorise('core.manage','com_joomlaquiz')){
+			if(!$user->authorise('core.manage')){
 				$query->where($db->qn('c_student_id').' = '.$db->q($user->id));
 			}
 			

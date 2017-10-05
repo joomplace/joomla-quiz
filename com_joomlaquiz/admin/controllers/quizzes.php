34fd47823fbfb16990d44a87ea84bf6cbf06956f
Vitaly Busel <v.busel@belitsoft.com>
2017-10-05 15:06:21 +0300

    Solving problem while importing quizzes

--- com_joomlaquiz/admin/controllers/quizzes.php
+++ com_joomlaquiz/admin/controllers/quizzes.php
@@ -840,7 +840,7 @@ class JoomlaquizControllerQuizzes extends JControllerAdmin
 								c_right_message, c_wrong_message, c_pass_message, 
 								c_unpass_message, c_enable_review, c_email_to, 
 								c_enable_print, c_enable_sertif, c_skin, 
-								c_random, c_guest, published, 
+								c_random, published, 
 								c_slide, c_language, c_certificate, 
 								c_feedback, c_pool, c_auto_breaks,
 								c_resbycat,	c_feed_option, paid_check, c_pagination)  ";
@@ -853,7 +853,7 @@ class JoomlaquizControllerQuizzes extends JControllerAdmin
 								".$db->quote($qcat->quiz_rmess).",".$db->quote($qcat->quiz_wmess).",".$db->quote($qcat->quiz_pass_message).",
 								".$db->quote($qcat->quiz_unpass_message).", ".$db->quote(@$qcat->quiz_enable_review).", ".$db->quote($qcat->quiz_email_to).",
 								".$db->quote($qcat->quiz_enable_print).",".$db->quote($qcat->quiz_enable_sertif).",".$db->quote($qcat->quiz_skin).",
-								".$db->quote($qcat->quiz_random).",".$db->quote($qcat->quiz_guest).",".$db->quote($qcat->quiz_published).",
+								".$db->quote($qcat->quiz_random).",".$db->quote($qcat->quiz_published).",
 								".$db->quote($qcat->quiz_slide).",".$db->quote($qcat->quiz_language).",".$db->quote($qcat->quiz_certificate).",
 								".$db->quote($qcat->quiz_feedback).",".$db->quote($qcat->quiz_pool).",".$db->quote(@$qcat->quiz_auto_breaks).",".$db->quote(@$qcat->quiz_resbycat).",
 								".$db->quote($qcat->quiz_feed_option).", ".$db->quote($qcat->quiz_paid_check).", ".$db->quote($qcat->quiz_pagination).")";

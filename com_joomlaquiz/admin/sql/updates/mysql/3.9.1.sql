1f0426eed5335a054db2753e530b7a7a70e53db7
Serguei Filitchonok <fsvpost@gmail.com>
2017-11-10 16:11:47 +0300

    sql5

--- com_joomlaquiz/admin/sql/updates/mysql/3.9.1.sql
+++ com_joomlaquiz/admin/sql/updates/mysql/3.9.1.sql
@@ -1,7 +1,8 @@
-delimiter ;;
+delimiter $$
 create procedure foo ()
   begin
     declare continue handler for 1060 begin end;
     alter table `#__quiz_t_quiz` add column `c_prob_total_q` INT(11) NOT NULL DEFAULT '0' AFTER `c_auto_breaks`;
   end;;
-call foo();;
+call foo()$$
+delimiter ;

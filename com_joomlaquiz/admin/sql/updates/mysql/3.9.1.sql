6d67f7996df48f4c456e8f2cdaab0feca63d7856
Serguei Filitchonok <fsvpost@gmail.com>
2017-11-10 16:03:42 +0300

    change sql 4

--- com_joomlaquiz/admin/sql/updates/mysql/3.9.1.sql
+++ com_joomlaquiz/admin/sql/updates/mysql/3.9.1.sql
@@ -1,10 +1,7 @@
-SELECT count(*)
-INTO @exist
-FROM information_schema.columns
-WHERE table_schema = database()
-      and COLUMN_NAME = 'c_prob_total_q'
-      AND table_name = '#__quiz_t_quiz';
-set @query = IF(@exist <= 0, 'alter table #__quiz_t_quiz add column c_prob_total_q INT(11) NOT NULL DEFAULT 0 AFTER c_auto_breaks',
-                'select \'Column Exists\' status');
-prepare stmt from @query;
-EXECUTE stmt;
+delimiter ;;
+create procedure foo ()
+  begin
+    declare continue handler for 1060 begin end;
+    alter table `#__quiz_t_quiz` add column `c_prob_total_q` INT(11) NOT NULL DEFAULT '0' AFTER `c_auto_breaks`;
+  end;;
+call foo();;

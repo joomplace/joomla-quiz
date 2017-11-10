1078180db191d3f1225cdc7591757c24b751f48e
Serguei Filitchonok <fsvpost@gmail.com>
2017-11-10 15:52:29 +0300

    change sql 3

--- com_joomlaquiz/admin/sql/updates/mysql/3.9.1.sql
+++ com_joomlaquiz/admin/sql/updates/mysql/3.9.1.sql
@@ -4,7 +4,7 @@ FROM information_schema.columns
 WHERE table_schema = database()
       and COLUMN_NAME = 'c_prob_total_q'
       AND table_name = '#__quiz_t_quiz';
-set @query = IF(@exist <= 0, 'alter table intent add column c_prob_total_q INT(11) NOT NULL DEFAULT 0 AFTER c_auto_breaks',
+set @query = IF(@exist <= 0, 'alter table #__quiz_t_quiz add column c_prob_total_q INT(11) NOT NULL DEFAULT 0 AFTER c_auto_breaks',
                 'select \'Column Exists\' status');
 prepare stmt from @query;
 EXECUTE stmt;

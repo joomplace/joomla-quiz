02a93055d791c3a2290df9d37db0d64c484e01e6
Serguei Filitchonok <fsvpost@gmail.com>
2017-11-10 15:36:06 +0300

    change sql

--- com_joomlaquiz/admin/sql/updates/mysql/3.9.1.sql
+++ com_joomlaquiz/admin/sql/updates/mysql/3.9.1.sql
@@ -1,1 +1,11 @@
-ALTER TABLE `#__quiz_t_quiz` ADD COLUMN `c_prob_total_q` INT(11) NOT NULL DEFAULT '0' AFTER `c_auto_breaks`;
+DELIMITER $$
+DROP PROCEDURE IF EXISTS upgrade_database_390_to_391 $$
+CREATE PROCEDURE upgrade_database_390_to_391()
+  BEGIN
+    IF NOT EXISTS( (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA=DATABASE()
+                                                                   AND COLUMN_NAME='c_prob_total_q' AND TABLE_NAME='#__quiz_t_quiz') ) THEN
+      ALTER TABLE `#__quiz_t_quiz` ADD COLUMN `c_prob_total_q` INT(11) NOT NULL DEFAULT '0' AFTER `c_auto_breaks`;
+    END IF;
+  END $$
+CALL upgrade_database_390_to_391() $$
+DELIMITER ;

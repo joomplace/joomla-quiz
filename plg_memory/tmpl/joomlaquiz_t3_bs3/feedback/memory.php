e1ec1cfc1cec01a7c9bfcb0d631ec9412d908a02
Levshunov <o.levshunov@belitsoft.com>
2018-12-11 15:16:34 +0300

    added new template t3_b3 for next questions such as imgmatch puzzle memory, and fixed path for img in review and feedback

---
+++ plg_memory/tmpl/joomlaquiz_t3_bs3/feedback/memory.php
@@ -0,0 +1,65 @@
+<?php
+/**
+* Joomlaquiz Deluxe Component for Joomla 3
+* @package Joomlaquiz Deluxe
+* @author JoomPlace Team
+* @copyright Copyright (C) JoomPlace, www.joomplace.com
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+*/
+
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Joomlaquiz Deluxe class
+ */
+class JoomlaquizViewFeedbackMemory
+{
+	public static function getFeedbackContent($feedback_data, $data){
+		
+		$jq_tmpl_html = "<div class='jq_feedback_question_content'>";
+		$jq_tmpl_html .= '<table class="jq_feedback_question_content_inner">
+							<tbody>
+							<tr class="jq_feedback_question_content_header">
+								<td class="jq_feedback_question_content_col_wide">'.JText::_('COM_JQ_POSSIBLE_ANSWERS').'</td>
+								<td class="jq_feedback_question_content_col_narrow">'.JText::_('COM_JQ_YOUR_CHOICE').'</td>
+								<td class="jq_feedback_question_content_col_narrow">'.JText::_('COM_JQ_YOUR_MEMORY_POINTS').'</td>';
+		
+		$k = 2;
+		$sum_score = 0;
+		if(count($feedback_data['memory_data'])){
+			foreach($feedback_data['memory_data'] as $mem) {
+				$wrong_correct = (in_array($mem->m_id, $feedback_data['udata'])) ? '<img src="'.JURI::root().'components/com_joomlaquiz/assets/images/tick.png" style="vertical-align:middle;margin-left:10px;"/>' : '<img src="'.JURI::root().'components/com_joomlaquiz/assets/images/tickr.png" style="vertical-align:middle;margin-left:10px;"/>';
+				
+				$expl = '';
+				if(in_array($mem->m_id, $feedback_data['udata'])){
+					$cc = array_search($mem->m_id, $feedback_data['udata']);
+					$take_time = $feedback_data['c_elapsed_times'][$cc];
+					$penalty = ($data['q_data']->c_sec_penalty) ? round($take_time/$data['q_data']->c_sec_penalty) * $data['q_data']->c_penalty : 0;
+					$score = $mem->a_points - $penalty;
+					$expl = ' ('.JText::_('COM_JOOMLAQUIZ_YOU_GOT').' '.$mem->a_points.' '.JText::_('COM_JOOMLAQUIZ_POINTS_MINUS').' '.$penalty.' '.JText::_('COM_JOOMLAQUIZ_PENALTY_POINTS').')';
+					if($score < 0){
+						$score = 0;
+					}
+				} else {
+					$score = 0;
+				}
+				
+				$jq_tmpl_html .= '<tr class="jq_feedback_question_content_container">'.
+								'<td class="jq_feedback_question_content_col_wide"><img src="'. JURI::root().'images/joomlaquiz/images/memory/'.($mem->c_img). '" width="150"/>&nbsp;&nbsp;<img src="'. JURI::root().'images/joomlaquiz/images/memory/'.($mem->c_img). '" width="150"/></td>'.
+								'<td class="jq_feedback_question_content_col_narrow">'.$wrong_correct.'</td>'.
+								'<td class="jq_feedback_question_content_col_narrow">'.$score.$expl.'</td>'.
+								'</tr>';
+				$k = 3 - $k;
+				$sum_score += $score;
+			}
+		}
+		
+		$jq_tmpl_html .= "<tr><td colspan='3' valign='top'><br /><strong>".JText::_('COM_QUIZ_RES_MES_SCORE')." ".$sum_score."</strong><br /></td>" . "\n";
+		$jq_tmpl_html .= "</tr></tbody></table>" . "\n";
+		$jq_tmpl_html.='</div>' . "\n";
+		
+		return $jq_tmpl_html;
+	}
+}
+
+?>
---
+++ plg_puzzle/tmpl/joomlaquiz_t3_bs3/review/puzzle.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Joomlaquiz Deluxe Component for Joomla 3
+ * @package Joomlaquiz Deluxe
+ * @author JoomPlace Team
+ * @copyright Copyright (C) JoomPlace, www.joomplace.com
+ * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Joomlaquiz Deluxe class
+ */
+class JoomlaquizViewReviewPuzzle
+{
+    public static function getReviewContent($review_data, $data){
+        $jq_tmpl_html = "<table align='left' class='jq_puzzle_fdb' width='100%'>" . "\n";
+        $jq_tmpl_html .= "<tr><td><div class='jq_puzzle_img' style='width:100%;position:relative;'>";
+
+        if(file_exists(JPATH_SITE.'/images/joomlaquiz/images/'.$data['qdata'][0]['q_data']->c_image)){
+            $img_data = getimagesize(JPATH_SITE.'/images/joomlaquiz/images/'.$data['qdata'][0]['q_data']->c_image);
+        }
+
+        $scale = 1.5;
+        $pieceWidth = round(($img_data[0] / $data['qdata'][0]['difficulty']) / $scale);
+        $pieceHeight = round( ($img_data[1] / $data['qdata'][0]['difficulty']) / $scale);
+        $puzzleWidth = $pieceWidth * $data['qdata'][0]['difficulty'];
+        $puzzleHeight = $pieceHeight * $data['qdata'][0]['difficulty'];
+
+        $xPos = 0;
+        $yPos = 0;
+
+        $jq_tmpl_html .= "<img src='".JURI::root()."images/joomlaquiz/images/".$data['qdata'][0]['q_data']->c_image."' width='".$puzzleWidth."' height='".$puzzleHeight."' style='position:absolute;top:0px;left:0px;'>";
+        $cc = 1;
+        for($n = 0; $n < $data['qdata'][0]['difficulty']; $n++){
+            for($m = 0; $m < $data['qdata'][0]['difficulty']; $m++){
+                $bkg = (!in_array($cc, $review_data)) ? 'background: #eeeeee;filter:alpha(opacity=70);-moz-opacity:.70; opacity:.70;' : 'filter:alpha(opacity=0);-moz-opacity:0; opacity:0;';
+                $jq_tmpl_html .= "<div style='".$bkg." width:".$pieceWidth."px; height:".$pieceHeight."px;float:left;'><!--x--></div>";
+                $xPos += $pieceWidth;
+                if($xPos >= $puzzleWidth){
+                    $xPos = 0;
+                    $yPos += $pieceHeight;
+                }
+                $cc++;
+            }
+            $jq_tmpl_html .= "<div style='clear:both;'><!--x--></div>";
+        }
+
+        $jq_tmpl_html .= "</div></td></tr>";
+        $jq_tmpl_html .= "</table>" . "\n";
+        return $jq_tmpl_html;
+    }
+}
+
+?>

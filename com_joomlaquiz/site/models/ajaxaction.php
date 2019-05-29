2d28f3dd3cec249eba7a5b251f1b6931e5f84894
zatulenko_d <d.zatulenko@belitsoft.com>
2019-05-29 11:55:52 +0300

    Added condition for issue type Boilerplate

--- com_joomlaquiz/site/models/ajaxaction.php
+++ com_joomlaquiz/site/models/ajaxaction.php
@@ -2327,7 +2327,7 @@ class JoomlaquizModelAjaxaction extends JModelList
 				$quest_id = $quest_ids[$q];
 				$answer = $answers[$q];
 				
-				if ($answer == '~~~'|| $answer == '') continue;
+				if ($answer == '~~~'|| $answer == '' || $answer == 0) continue;
 				
 				// get question type
 				$query = "SELECT c_type from #__quiz_t_question WHERE c_id = '".$quest_id."' AND published = 1";

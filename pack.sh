1047a304884f6aa9247648badc99d466decef717
Alexandr Kosarev <alex7r@ya.ru>
2019-08-10 13:21:01 +0300

    test/update

--- pack.sh
+++ pack.sh
@@ -9,11 +9,15 @@ sed -E 's/<file[^>]+>|.zip<\/file>//g' | \
 while read in;
 do
 if [ "$in" != 'plg_installer_joomplaceupdater' ]; then
+    echo "---------------";
+    pwd;
+    ls -la;
     ls -la "$in";
     cd ./"$in";
 #    zip -r $PWD/"$in".zip ./*;
     echo "Packed $in";
     pwd;
+    echo "---------------";
     cd $PWD;
 fi;
 done

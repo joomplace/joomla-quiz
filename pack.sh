c54df490a90d1b4f453fd9f1830196108520afd0
Alexandr Kosarev <alex7r@ya.ru>
2019-08-10 13:16:10 +0300

    test/update sh

--- pack.sh
+++ pack.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 PWD=`pwd`
+ls -la
 grep -E -o '<file.*>(.*?)</file>' ./pkg_quiz.xml | \
 sed -E 's/<file[^>]+>|.zip<\/file>//g' | \
 while read in;
 do
 if [ "$in" != 'plg_installer_joomplaceupdater' ]; then
     cd ./"$in";
-    zip -r $PWD/"$in".zip ./*;
+#    zip -r $PWD/"$in".zip ./*;
     cd $PWD;
 fi;
 done

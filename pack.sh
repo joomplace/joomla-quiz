ff0cbd13689c555a9282030b6a20af6329528882
Alexandr Kosarev <alex7r@ya.ru>
2019-08-10 13:20:03 +0300

    test/update

--- pack.sh
+++ pack.sh
@@ -12,6 +12,8 @@ if [ "$in" != 'plg_installer_joomplaceupdater' ]; then
     ls -la "$in";
     cd ./"$in";
 #    zip -r $PWD/"$in".zip ./*;
+    echo "Packed $in";
+    pwd;
     cd $PWD;
 fi;
 done

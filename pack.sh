768523bb174d10828a49996708889c618e3ba303
Alexandr Kosarev <alex7r@ya.ru>
2019-08-10 13:18:01 +0300

    test/update

--- pack.sh
+++ pack.sh
@@ -2,11 +2,14 @@
 
 PWD=`pwd`
 ls -la
+ls -la ./module
+ls -la ./plugin
 grep -E -o '<file.*>(.*?)</file>' ./pkg_quiz.xml | \
 sed -E 's/<file[^>]+>|.zip<\/file>//g' | \
 while read in;
 do
 if [ "$in" != 'plg_installer_joomplaceupdater' ]; then
+    ls -la "$in";
     cd ./"$in";
 #    zip -r $PWD/"$in".zip ./*;
     cd $PWD;

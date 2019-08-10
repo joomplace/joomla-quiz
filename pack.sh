f67540ab82330c0a05bbf698e4bc293086fae4e0
Alexandr Kosarev <alex7r@ya.ru>
2019-08-10 13:28:36 +0300

    test/update

--- pack.sh
+++ pack.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 
 STARTING_PATH=`pwd`
-ls -la
-ls -la ./module
-ls -la ./plugin
 grep -E -o '<file.*>(.*?)</file>' ./pkg_quiz.xml | \
 sed -E 's/<file[^>]+>|.zip<\/file>//g' | \
 while read in;
 do
 if [ "$in" != 'plg_installer_joomplaceupdater' ]; then
-    echo "---------------";
-    echo "Packing $in";
-    echo "---------------";
     cd ./"$in";
-    zip -r $STARTING_PATH/"$in".zip ./*;
-    echo "Packed $in ✔";
+    zip -r "$STARTING_PATH/$in".zip ./*;
+    echo "✔ $in ";
     cd "$STARTING_PATH";
 fi;
 done

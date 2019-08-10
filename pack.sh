dda4e59efa28bcb62e08935552a6ab6292c2f6f3
Alexandr Kosarev <alex7r@ya.ru>
2019-08-10 14:08:58 +0300

    test/update

--- pack.sh
+++ pack.sh
@@ -7,11 +7,9 @@ while read in;
 do
 if [ "$in" != 'plg_installer_joomplaceupdater' ]; then
     cd ./"$in";
-    if [ ! -f "$STARTING_PATH/packages/$in".zip ]; then
-        mkdir -p "$STARTING_PATH/packages/$in";
-        rm -rf "$STARTING_PATH/packages/$in";
-    fi
-    zip -qtr "$STARTING_PATH/packages/$in".zip ./*;
+    mkdir -p "$STARTING_PATH/packages/$in";
+    rm -rf "$STARTING_PATH/packages/$in";
+    zip -qr "$STARTING_PATH/packages/$in".zip ./*;
     echo "✔ $in";
     cd "$STARTING_PATH";
 fi;

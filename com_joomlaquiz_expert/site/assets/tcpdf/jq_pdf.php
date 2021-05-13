db11b28dc7e5c9e7407937d83e6057837cf7e7a6
filitchonok_s <s.filitchonok@belitsoft.com>
2021-05-13 20:00:39 +0300

    refactoring

--- com_joomlaquiz_expert/site/assets/tcpdf/jq_pdf.php
+++ com_joomlaquiz_expert/site/assets/tcpdf/jq_pdf.php
@@ -130,7 +130,22 @@ class jq_pdf
 		$this->_engine->setHeaderData('', 0, $config->sitename, $config->live_site." . "._PDF_GENERATED .' '. JHtml::_('date', time() , 'j F, Y, H:i' ) );	
 		
 		// Set PDF Header and Footer fonts
-		$font = 'dejavusans';
+        $lang = \JFactory::getLanguage()->getTag();
+        $alt_lang = array(
+            'ar-AA', //Arabic (Unitag)
+            'ar-SA', //Arabic (Saudi Arabia)
+            'he-IL', //Hebrew (Israel)
+            'ja-JP', //Japanese (Japan)
+            'zh-CN', //Chinese (China)
+            'zh-HK', //Chinese (Hong Kong)
+            'zh-TW'  //Chinese (Taiwan)
+        );
+
+        if(in_array($lang, $alt_lang)){
+            $font = 'javiergb';
+        } else {
+            $font = 'dejavusans';
+        }
 
 		$this->_engine->setHeaderFont(array($font, '', 7));
 		$this->_engine->setFooterFont(array($font, '', 7));
@@ -140,6 +155,7 @@ class jq_pdf
 	function cleanText($text)
     {
 		$text = trim(strip_tags($text));
+        $text = preg_replace('/^\s+$/m', '', $text);
 		$text = str_replace("\t",'', $text);
 		$text = str_replace("&nbsp;",' ', $text);
 		$text = $this->decodeHTML($text);

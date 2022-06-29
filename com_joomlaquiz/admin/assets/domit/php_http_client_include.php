25753c26c9de0f45dad18bb805cf12defd717dce
Serguei Filitchonok <32449884+fsvblr@users.noreply.github.com>
2022-06-29 13:36:10 +0300

    deleting the standard version of a package

--- com_joomlaquiz/admin/assets/domit/php_http_client_include.php
+++
@@ -1,9 +0,0 @@
-<?php defined('_JEXEC') or die;
-
-/**
- * @license http://www.gnu.org/copyleft/lesser.html LGPL License
- **/
-
-define('PHP_HTTP_TOOLS_INCLUDE_PATH', (dirname(__FILE__) . "/"));
-require_once(PHP_HTTP_TOOLS_INCLUDE_PATH . 'php_http_client_generic.php');
-?>
